16bd7ebc7c32ac6e8589777cd4bc50caa09f9475
Paulo Abdiel <abdielpaulo2@gmail.com>
2025-04-11 22:37:19 -0300

    fix: update

--- src/routes/dashboard/_authenticated/home.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import Dashboard from '@/pages/dashboard/home'
-import { createLazyFileRoute } from '@tanstack/react-router'
-
-export const Route = createLazyFileRoute('/dashboard/_authenticated/home')({
-  component: Dashboard,
-})
